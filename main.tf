1f3c354a2873ae31d011bf2e80d757a09910d18d
satya <satyasairam94@gmail.com>
2023-02-07 11:25:19 +0530

    added new ec2 for testing

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
-resource "aws_s3_bucket" "new_bucket" {
-  bucket = "testings3bucketinsingaporeregion12345678"
+#resource "aws_s3_bucket" "new_bucket" {
+#  bucket = "testings3bucketinsingaporeregion12345678"
+#
+#  tags = {
+#    Name        = "testing"
+#
+#  }
+#}
+#provider "aws" {
+#  region = "ap-south-1"
+#}
+
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-01a4f99c4ac11b03c"
+  instance_type = "t2.micro"
 
   tags = {
-    Name        = "testing"
-
+    Name = "namechange"
   }
-}
-provider "aws" {
-  region = "ap-south-1"
 }
