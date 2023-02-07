e2778f7157b52250a41bdf31c703bff9f99a6fd4
satya <satyasairam94@gmail.com>
2023-02-07 11:41:44 +0530

    added new s3buckets for testing

--- main.tf
+++ main.tf
@@ -1,22 +1,26 @@
-#resource "aws_s3_bucket" "new_bucket" {
-#  bucket = "testings3bucketinsingaporeregion12345678"
-#
-#  tags = {
-#    Name        = "testing"
-#
-#  }
-#}
-#provider "aws" {
-#  region = "ap-south-1"
-#}
-
-
-
-resource "aws_instance" "web" {
-  ami           = "ami-01a4f99c4ac11b03c"
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "new_bucket" {
+  count = length(var.s3_bucketnames)
+  bucket = element(var.s3_bucketnames, count.index )
 
   tags = {
-    Name = "namechange"
+    Name        = "testing"
+
   }
 }
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+
+#
+#resource "aws_instance" "web" {
+#  ami           = "ami-01a4f99c4ac11b03c"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "namechange"
+#  }
+#}
